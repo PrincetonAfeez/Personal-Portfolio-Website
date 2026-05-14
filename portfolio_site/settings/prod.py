707b1ba6c4c55cc958efb925af4be00b16ac8ec8
Princeton Afeez <princetonafeez@gmail.com>
2026-05-14 10:04:20 -0700

    Production settings for the portfolio site

---
+++ portfolio_site/settings/prod.py
@@ -0,0 +1,14 @@
+"""Production settings for the portfolio site."""
+
+from .base import *  # noqa: F403
+
+
+DEBUG = False
+
+SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", default=True)  # noqa: F405
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = env.int("SECURE_HSTS_SECONDS", default=31_536_000)  # noqa: F405
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
