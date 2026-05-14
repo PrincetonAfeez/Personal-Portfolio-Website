e0551ed6fe6906977a053a2e5de3c6e1aed742e5
Princeton Afeez <princetonafeez@gmail.com>
2026-05-14 10:07:40 -0700

    URL configuration for portfolio_site project

---
+++ portfolio_site/urls.py
@@ -0,0 +1,48 @@
+"""
+URL configuration for portfolio_site project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.urls import include, path
+
+from core.sitemaps import StaticViewSitemap
+from core import views as core_views
+from projects.sitemaps import ProjectSitemap
+
+sitemaps = {
+    "static": StaticViewSitemap,
+    "projects": ProjectSitemap,
+}
+
+urlpatterns = [
+    path("", core_views.home, name="home"),
+    path("admin/", admin.site.urls),
+    path("projects/", include("projects.urls")),
+    path("about/", core_views.about, name="about"),
+    path("contact/", include("contact.urls")),
+    path("resume/", core_views.resume, name="resume"),
+    path("robots.txt", core_views.robots_txt, name="robots"),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+]
+
+handler404 = "core.views.handler404"
+handler500 = "core.views.handler500"
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
