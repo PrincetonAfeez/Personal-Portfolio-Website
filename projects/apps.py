acb6ad53bbc5b5cca0025ac4f4ac83c4204dcc08
Princeton Afeez <princetonafeez@gmail.com>
2026-05-14 10:15:44 -0700

    App configuration for the projects app

---
+++ projects/apps.py
@@ -0,0 +1,8 @@
+"""App configuration for the projects app."""
+
+from django.apps import AppConfig
+
+
+class ProjectsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "projects"
